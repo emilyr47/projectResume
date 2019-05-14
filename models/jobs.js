215328406c5a60e849d9d2f501317660ea94f156
mityapetrenko <dimbaslv@gmail.com>
2019-05-13 19:29:42 -0500

    validation

--- models/jobs.js
+++ models/jobs.js
@@ -1,14 +1,41 @@
 module.exports = function(sequelize, DataTypes) {
   var Jobs = sequelize.define("Jobs", {
-    company: DataTypes.STRING,
-    position: DataTypes.STRING,
-    appliedDate: DataTypes.DATE,
-    contactInfo: DataTypes.TEXT,
-    resumeLink: DataTypes.STRING,
-    interviewDate: DataTypes.DATE,
-    interviweeName: DataTypes.STRING,
-    jobOffered: DataTypes.BOOLEAN,
-    comments: DataTypes.TEXT
+    company:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate:{ len: [1,140]}
+    },
+    position:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate:{ len: [1,140]}
+    },
+    appliedDate:{
+     type: DataTypes.DATE,
+     isDate: true
+    },
+    contactInfo:{
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    resumeLink:{
+      type:DataTypes.STRING,
+    },
+    interviewDate:{
+      type: DataTypes.DATE,
+      isDate: true
+    },
+    interviweeName: {
+      type:DataTypes.STRING,
+    },
+    jobOffered:{
+      type:DataTypes.BOOLEAN,
+      defaultValue:false
+    } ,
+    comments:{
+      type: DataTypes.TEXT,
+      
+    }
   });
   return Jobs;
 };
